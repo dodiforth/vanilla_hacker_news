fd71ec528d6e104b570f1bc4b5a10f806ee36843
Dowon Kim <dowoniscool@yahoo.com>
2024-05-05 16:57:37 +0200

    Update main.js to fetch and display titles from Hacker News API

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-const ajax = new XMLHttpRequest();
+const baseURL = 'https://hacker-news.firebaseio.com/v0/item/';
+const itemNumbers = Array.from({length: 10}, (_, i) => i + 1);
 
-ajax.open('GET', 'https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty', false);
-ajax.send();
+const fetchPromises = itemNumbers.map(num => 
+  fetch(`${baseURL}${num}.json`).then(response => response.json())
+);
 
-console.log(ajax.responseText);
+Promise.all(fetchPromises)
+  .then(items => items.map(item => item.title))
+  .then(titles => {
+    const ul = document.createElement('ul'); // Create a new <ul> element
+    titles.forEach(title => {
+      const li = document.createElement('li'); // Create a new <li> element
+      li.textContent = title; // Set the text content of the <li> element to the title
+      ul.appendChild(li); // Append the <li> element to the <ul> element
+    });
+    document.body.appendChild(ul); // Append the <ul> element to the body of the HTML document
+  })
+  .catch(error => console.error(error));
